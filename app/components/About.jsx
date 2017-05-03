13d7a44f4d354de0176a59f907e60683e404a959
sazjan <gurungsazjan@gmail.com>
2017-05-03 13:46:26 +1000

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,30 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function(){
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-left">About</h1>
+      <p>
+        This is the weather appliation built on react. I have built this
+        following the complete react web app developer course. I have learned
+        a lot of following this tutorial.
+      </p>
+      <p>Things I learned:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> -The javascript
+          framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Fetching api</a> - I learned to
+          fetch api from website (in this case open weather map) and utilize
+          in this web application.
+        </li>
+        <li>
+          <a href="https://www.heroku.com">Heroku</a> - I learned to deploy the
+          application in the web using this cloud application platform.
+        </li>
+      </ul>
     </div>
   )
 };
