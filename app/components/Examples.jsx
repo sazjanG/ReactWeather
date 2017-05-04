d19b194b1e9fd70a125be670d3f2cf47a0ebcdbf
sazjan <gurungsazjan@gmail.com>
2017-05-04 11:53:37 +1000

    add nav search functionality

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,7 +8,7 @@ var Examples = (props) => {
       <p>Here are few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Melbourne'>Melborne</Link>
+          <Link to='/?location=Melborne'>Melbourne</Link>
         </li>
         <li>
           <Link to='/?location=Kathmandu'>Kathmandu, Nepal</Link>
